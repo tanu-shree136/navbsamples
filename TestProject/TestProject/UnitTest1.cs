f2c08a69c83ff17cf74618f4b2d1caf0f2fab1ea
tanu-shree136 <41438291+tanu-shree136@users.noreply.github.com>
2018-12-12 12:07:51 +0530

    Update UnitTest1.cs

--- TestProject/TestProject/UnitTest1.cs
+++ TestProject/TestProject/UnitTest1.cs
@@ -6,22 +6,36 @@ namespace TestProject
     [TestClass]
     public class UnitTest1
     {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+        }
         [TestMethod]
         public void TestMethod1()
         {
-            System.Threading.Thread.Sleep(10*1000);
+            this.TestContext.AddResultFile("D:/LogStore0.zip"); 
+            Assert.IsTrue(true);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            System.Threading.Thread.Sleep(10*1000);
+            Assert.IsTrue(true);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            System.Threading.Thread.Sleep(10*1000);
+            Assert.IsTrue(true);
         }
     }
 }
